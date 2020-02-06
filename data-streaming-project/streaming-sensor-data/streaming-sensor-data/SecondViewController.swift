4d6118f388d5fe021184e2f1e1ec154fbb615d79
Brandon Hancock <brandon.e.hancock@outlook.com>
2020-02-06 00:42:36 -0800

    Start of project

---
+++ data-streaming-project/streaming-sensor-data/streaming-sensor-data/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  streaming-sensor-data
+//
+//  Created by Brandon Hancock on 2/6/20.
+//  Copyright © 2020 Brandon Hancock. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
